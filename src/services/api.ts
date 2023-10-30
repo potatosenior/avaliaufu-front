a275c579ccb0d3ca5028fff5519d459ff2dbbd6b
Joao Pedro <joao.vieira@bitcapital.com.br>
2023-10-30 01:07:13 -0300

    skip ngrok

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,3 +5,9 @@ import { setupInterceptors } from './interceptors'
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
+api.interceptors.request.use((request) => {
+  // add "ngrok-skip-browser-warning": "69420" to requests headers
+  request.headers['ngrok-skip-browser-warning'] = '69420'
+
+  return request
+})
